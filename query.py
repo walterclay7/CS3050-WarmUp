eae15ff419dfb41c58da60772f1006adf42fae53
rohbot25 <ewrohan@gmail.com>
2024-09-10 12:03:39 -0400

    functions

--- query.py
+++ query.py
@@ -1,1 +1,13 @@
 from google.cloud import firestore
+
+#get the info from the parser
+def get_info():
+    pass
+
+#query the firestore RETURN true or false weather or not the data exists
+def query():
+    pass
+
+#print appropriate information
+def printToTerminal():
+    pass
