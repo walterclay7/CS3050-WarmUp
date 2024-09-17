9b23cd51093c99ca3d4af0b2c4d4bb5ed02f93a2
owencook <oacook@uvm.edu>
2024-09-17 12:18:14 -0400

    Working with reading data from firestore

--- query.py
+++ query.py
@@ -1,14 +1,30 @@
-from google.cloud import firestore
+from config_firestore import db
+import firebase_admin
+from google.cloud.firestore_v1 import FieldFilter
 
-#get the info from the parser
-def get_info():
+def query_retrieve(keyword, operator, user_ln):
     pass
+    # Check for 1 or 2 queries (maybe recursion)
 
-#query the firestore RETURN true or false weather or not the data exists
-def query():
-    pass
+    # Hard code 4 cases
 
-#print appropriate information
-def printToTerminal():
-    pass
+    # Single queries
 
+    # Compund queries
+
+    # The ALL
+
+    # Return list of all books
+
+
+
+
+docs = (db.collection('books')
+        .where(filter=FieldFilter('author', '==', 'J.R.R. Tolkien'))
+        )
+
+docs2 = (docs.where(filter=FieldFilter('published_year', '<', 1940))
+         .stream())
+# Set of (remove duplicates)
+for doc in docs2:
+    print(doc.to_dict())
